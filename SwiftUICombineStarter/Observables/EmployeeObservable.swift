e85a5c6534c8727955d63289069196f89b888de3
Khasnobis, Pritha (US - Bengaluru) <pkhasnobis@deloitte.com>
2020-05-06 12:57:25 +0530

    "API call code"

--- SwiftUICombineStarter/Observables/EmployeeObservable.swift
+++ SwiftUICombineStarter/Observables/EmployeeObservable.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+final class EmployeeObservable: ObservableObject {
+
+    static let shared = EmployeeObservable()
+    private init() {
+       // fetchEmployees()
+    }
+    
+    var employeeArray = [Employee]()
+    var error = ""
+    var isLoading = false
+
+    private func fetchEmployees() {
+    
+        isLoading = true
+        
+        guard let url = URL(string: "http://dummy.restapiexample.com/api/v1/employees") else {
+            
+            self.error = "Invalid URL"
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: url) { [unowned self] (data, response, error) in
+            self.isLoading = false
+            guard let data = data else {
+                self.error = error?.localizedDescription ?? "Error occured!"
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            do {
+                let employeeData = try jsonDecoder.decode(EmployeeData.self, from: data)
+                if let data = employeeData.data {
+                    self.employeeArray = data
+                }
+            } catch {
+                self.error = error.localizedDescription
+            }
+        }
+        task.resume()
+    }
+}
